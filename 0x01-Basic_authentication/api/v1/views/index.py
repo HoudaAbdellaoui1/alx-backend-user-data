45dfebe46bec3ce1215678234bdfc7470a2faedb
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-11-15 23:15:22 +0100

    Implement Unauthorized ENDPOINT

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() :
+    """ GET /api/v1/unauthorized
+    Return:
+      - abort request after unauthorized
+    """
+    abort(401)
