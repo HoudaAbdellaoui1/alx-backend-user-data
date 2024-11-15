3905482d251adad8a8f4a78891fbb102050b3a8c
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-11-16 00:50:17 +0100

    implement basic_auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,6 +18,9 @@ auth = None
 AUTH_TYPE = getenv('AUTH_TYPE', None)
 if(AUTH_TYPE == 'auth'):
     auth = Auth()
+
+elif(AUTH_TYPE == 'basic_auth'):
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
