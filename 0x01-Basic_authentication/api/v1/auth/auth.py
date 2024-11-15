0a125afe5edbb235a80e680de15d898810c0a38a
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-11-15 23:48:20 +0100

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,7 @@ from models import user
 class Auth():
     """ Auth class for handling authentication
     """
-    
+
     @classmethod
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determines if a path requires authentication.
@@ -21,10 +21,9 @@ class Auth():
         """Retrieves the Authorization header from the request
         """
         return None
-    
+
     @classmethod
     def current_user(self, request=None) -> TypeVar('User'):
         """ Returns current user
         """
         return None
-
