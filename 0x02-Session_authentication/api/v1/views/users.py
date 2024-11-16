2b598cc3d2f660223b8b9b3c706d7a67546ac094
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-11-17 00:41:21 +0100

    Retrieve authenticated user

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -8,8 +8,7 @@ from models.user import User
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
-    """
-    GET /api/v1/users
+    """ GET /api/v1/users
     Return:
         - A JSON response containing a list of all User objects.
     """
@@ -19,8 +18,7 @@ def view_all_users() -> str:
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
-    """
-    GET /api/v1/users/<user_id>
+    """ GET /api/v1/users/<user_id>
     Path parameter:
         - user_id (str): The ID of the User.
     Return:
@@ -41,8 +39,7 @@ def view_one_user(user_id: str = None) -> str:
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
-    """
-    DELETE /api/v1/users/<user_id>
+    """ DELETE /api/v1/users/<user_id>
     Path parameter:
         - user_id (str): The ID of the User.
     Return:
@@ -60,8 +57,7 @@ def delete_user(user_id: str = None) -> str:
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user() -> str:
-    """
-    POST /api/v1/users/
+    """ POST /api/v1/users/
     JSON body:
         - email (str): The email address of the User (required).
         - password (str): The password for the User (required).
@@ -100,8 +96,7 @@ def create_user() -> str:
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id: str = None) -> str:
-    """
-    PUT /api/v1/users/<user_id>
+    """ PUT /api/v1/users/<user_id>
     Path parameter:
         - user_id (str): The ID of the User.
     JSON body:
