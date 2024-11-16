7b0d1f193c4fa4f74989507584181dbace2bf5ba
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-11-16 01:32:40 +0100

    return credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -59,3 +59,31 @@ class BasicAuth(Auth):
             return None
 
         return decoded_string
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """
+        Extracts the user email and password from the decoded
+        Base64 string of the Authorization header.
+
+        Args:
+            decoded_base64_authorization_header (str): The decoded
+            Base64 part of the Authorization header.
+
+        Returns:
+            tuple: A tuple containing the user email and password,
+            or (None, None) if invalid.
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+
+        email, password = decoded_base64_authorization_header.split(":", 1)
+
+        return email, password
