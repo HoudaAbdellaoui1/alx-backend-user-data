c039fdd98b1c179c4d2d6866cad48710b43b55d9
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-11-17 02:14:13 +0100

    Add login to excluded

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -61,12 +61,13 @@ def before_request():
     if not auth.require_auth(request.path, excluded):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and auth.session_cookie(
+            request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
         abort(403)
-    
+
     if auth.authorization_header(request) and auth.session_cookie(request):
         abort(401)
         return None
