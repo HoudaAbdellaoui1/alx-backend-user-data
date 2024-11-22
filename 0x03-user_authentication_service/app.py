a4d8f51b62c721eb1a622be7bdb79b3ac097b3c4
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-11-23 00:11:19 +0100

    save session_id

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,7 +62,7 @@ def login() -> str:
     if not auth.valid_login(email, password):
         abort(401)
     session_id = auth.create_session(email)
-    response = jsonify({'email': email, 'message': "logged in"})
+    response = make_response(jsonify({'email': email, 'message': "logged in"}))
     response.set_cookie("session_id", session_id)
     return response
 
