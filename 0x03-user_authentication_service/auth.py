3e20dd0ad55eabfcbb0e4c9ea36d5cb7a5e9eb7a
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-11-22 23:36:34 +0100

    generate uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,6 +15,14 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(bytes, salt)
 
+def _generate_uuid() -> str:
+    """
+    Generate a new UUID and return its string representation.
+    
+    Returns:
+        str: A string representation of a new UUID.
+    """
+    return str(uuid.uuid4())
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -44,12 +52,3 @@ class Auth:
             return bcrypt.checkpw(password.encode(), user.hashed_password)
         except NoResultFound:
             return False
-
-    def _generate_uuid() -> str:
-        """
-        Generate a new UUID and return its string representation.
-        
-        Returns:
-            str: A string representation of a new UUID.
-        """
-        return str(uuid.uuid4())
