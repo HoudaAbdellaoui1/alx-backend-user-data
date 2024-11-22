a0e691dbd4abdab909aa31ae1a6abb2d7c0ee22a
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-11-23 00:44:58 +0100

    log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,6 +56,17 @@ def logout() -> str:
     auth.destroy_session(user.id)
     return redirect("/", code=302)
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """ Get user profile
+    """
+    session_id = request.cookies.get("session_id")
+    user = auth.get_user_from_session_id(session_id)
+    if user is None or not session_id:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5001")
