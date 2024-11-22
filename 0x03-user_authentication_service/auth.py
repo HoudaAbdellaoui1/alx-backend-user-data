664ed76501b18405c73238c5ddf710a61a89b928
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-11-23 00:39:47 +0100

    log out

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -82,7 +82,6 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """Destroy a session
         """
-        try:
-            self._db.update_user(user_id, session_id=None)
-        except NoResultFound:
+        if user_id is None:
             return None
+        self._db.update_user(user_id, session_id=None)
